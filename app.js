474fd350bb4a4a16a4e413c4f6796d7c10fab5c3
Sudhanshu Ranjan <77230416+SudhansuuRanjan@users.noreply.github.com>
2022-01-16 22:31:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,38 +2,45 @@ const PORT = process.env.PORT || 3000
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
-
 const app = express()
 
-const url = 'https://coinmarketcap.com/'
 
+const sites = [
+    {
+        name: 'coinmarketcap',
+        address: 'https://coinmarketcap.com/',
+        base: ''
+    },
+]
 
+const coins = []
 
-axios(url)
+sites.forEach(site => {
+  axios.get(site.address)
     .then(response => {
     const html = response.data
-    const coins = []
     const $ = cheerio.load(html);
 
         $('tr', html).each(function(){
             const coinName =  $(this).find('.iworPT').text()
             const price = $(this).find('.cLgOOr').text()
             const unit = $(this).find('.gGIpIK').text()
+
             coins.push({
                 coinName,
                 price,
                 unit
             })
         })
-
-  app.get('/coins', function (req, res) {
-    res.send(coins.splice(1,10))
-    })
-
-}).catch(err => console.log(err))
+})
+})
 
 app.get('/', (req, res) => {
     res.json('Welcome to  Crypto API. Add /coins at the end to get cryptocurrency live prices')
+})
+
+app.get('/coins', function (req, res) {
+    res.json(coins.slice(1,10))
 })
 
 
